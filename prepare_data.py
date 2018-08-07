56d230fa72955cd0f44911124a436a60455d83cf
Moritz Heusinger <moritz.heusinger@gmail.com>
2018-08-07 22:26:20 +0200

    prepared gsmc and electricity
    -prepared the two real world streams
    -added stream from these files

--- prepare_data.py
+++ prepare_data.py
@@ -7,38 +7,55 @@ Created on Tue Aug  7 13:11:46 2018
 """
 
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
+import numpy as np
 
 """This file prepares the real world data streams by reading the csv,
 doing One hot encoding, standardization, removing the id column,
 replace missing values and writing it to a new csv-file"""
 
-airlines_df = pd.read_csv('datasets/airlines.csv') # do all
-cover_df = pd.read_csv('datasets/covtypeNorm.csv') # remove id first column
+#airlines_df = pd.read_csv('datasets/airlines.csv') # do all
+#cover_df = pd.read_csv('datasets/covtypeNorm.csv') # remove id first column
 electricity_df = pd.read_csv('datasets/elecNormNew.csv') # remove id, ohe for label
-kdd_df = pd.read_csv('datasets/kddcup.data_10_percent_corrected.csv') # do all
-poker_df = pd.read_csv('datasets/poker-lsn.csv') # normalize
+#kdd_df = pd.read_csv('datasets/kddcup.data_10_percent_corrected.csv') # do all
+#poker_df = pd.read_csv('datasets/poker-lsn.csv') # normalize
+gmsc_df = pd.read_csv('datasets/gmsc-train.csv')
 
 """Drop id column"""
-airlines_df = airlines_df.drop(['id'], axis=1)
-cover_df = cover_df.drop(['id'], axis=1)
+#airlines_df = airlines_df.drop(['id'], axis=1)
+#cover_df = cover_df.drop(['id'], axis=1)
 electricity_df = electricity_df.drop(['id'], axis=1)
 #kdd_df = kdd_df.drop(['id'], axis=1)
-poker_df = poker_df.drop(['id'], axis=1)
+#poker_df = poker_df.drop(['id'], axis=1)
+
+gmsc_labels = gmsc_df['SeriousDlqin2yrs']
+gmsc_df = gmsc_df.drop(['SeriousDlqin2yrs'], axis=1)
+gmsc_df = gmsc_df.drop(gmsc_df.columns[0], axis=1)
+gmsc_df = gmsc_df.join(gmsc_labels)
+gmsc_df = gmsc_df.dropna(how='all')
 
 """Replace NaN"""
-# Currently no dataset has NaN
+gmsc_df = gmsc_df.replace([np.inf, -np.inf], np.nan).dropna(axis=0)
 
 """OHE"""
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import OneHotEncoder
+#from sklearn.preprocessing import LabelEncoder
+#from sklearn.preprocessing import OneHotEncoder
+#
+#ohe = pd.get_dummies(airlines_df[['Airline', 'AirportFrom', 'AirportTo']])
+#airlines_df = airlines_df.drop(['Airline', 'AirportFrom', 'AirportTo'], axis=1)
+#airlines_df = airlines_df.join(ohe)
+## poker_df is numeric
 
-ohe = pd.get_dummies(airlines_df[['Airline', 'AirportFrom', 'AirportTo']])
-airlines_df = airlines_df.drop(['Airline', 'AirportFrom', 'AirportTo'], axis=1)
-airlines_df = airlines_df.join(ohe)
-# poker_df is numeric
-
-
+elec_classes = ['UP', 'DOWN']
+electricity_df['class'] = electricity_df['class'].apply(elec_classes.index)
 
 """Standardization"""
+#stdsc = StandardScaler()
+#X, y = gmsc_df.iloc[:, 1:].values, gmsc_df.iloc[:, 0].values
+#X_std = stdsc.fit_transform(X)
 
 """Save as csv"""
+electricity_df.to_csv('datasets/electricity_final.csv')
+gmsc_df.to_csv('datasets/gmsc_final.csv')
+#poker_df.to_csv('datasets/poker_final.csv')
+#cover_df.to_csv('datasets/cover_final.csv')
--- streams-thesis.py
+++ streams-thesis.py
@@ -9,20 +9,26 @@ Created on Tue Aug  7 12:58:22 2018
 from skmultiflow.data.file_stream import FileStream
 from skmultiflow.classification.trees import HoeffdingTree
 from skmultiflow.evaluation.evaluate_prequential import EvaluatePrequential
+import pandas as pd
+from rslvq_stream import RSLVQ
 
 """init stream"""
-stream = FileStream(filepath='datasets/airlines.csv', target_idx=-1, 
-                    n_targets=1, cat_features_idx=[1, 3, 4])
+#stream = FileStream('datasets/electricity_final.csv', target_idx=-1, 
+#                    n_targets=1, cat_features_idx=[0, 2, 4, 6, 8])
+
+stream = FileStream('datasets/gmsc_final.csv', target_idx=-1, 
+                    n_targets=1)
 
 stream.prepare_for_use()
 
 """init clf"""
-clf = HoeffdingTree()
+clf = [HoeffdingTree(), RSLVQ(prototypes_per_class=8, gradient_descent='RMSprop')]
 
 """eval stream"""
 evaluator = EvaluatePrequential(show_plot=True, # this will also slow down the process
                                 pretrain_size=1,
-                                max_samples=50000,
-                                metrics=['performance', 'kappa', 'true_vs_predicts'])
+                                max_samples=100000,
+                                metrics=['performance', 'kappa_t'],
+                                output_file=None)
 
-evaluator.evaluate(stream=stream, model=clf)
+evaluator.evaluate(stream=stream, model=clf, model_names=['HTree', 'RSLVQ'])
